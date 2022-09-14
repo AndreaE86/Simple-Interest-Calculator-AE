28689dd0c0569d1e129b5b63b0215eb55831afc7
AndreaE86 <68388003+AndreaE86@users.noreply.github.com>
2022-09-14 19:15:09 +0200

    Update script.js
    I updated the JavaScript code, too.

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //accesses value of principal from element ID
+	p = document.getElementById("principal").value;
+ 
+    //assigns value of amount user input to var principal
+ 	var principal = document.getElementById("principal").value;
+ 
+    //validates entry for amount and determines entry is not zero or negative
+    //returns alert for invalid input
+	if ( principal <= 0 )
+	{
+    alert("Enter a positive number");
+    principal.focus();
+    return false;
+	} 
     
+    //more variables for values of rate and years
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+ 
+	//calculates interest for saving money
+	var interest = principal * years * rate /100;
+  
+    //current date plus the years selected assigned to variable
+	var year = new Date().getFullYear()+parseInt(years);
+
+    //displays complete output of calculation to user
+	document.getElementById("result").innerHTML= "\<br\>\<br\>If you deposit <mark>"+principal+"</mark>\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\> You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>"
 }
-        
+
+function updateRate() 
+{
+    //enables slider rate to change dynamically 
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
+
+  
