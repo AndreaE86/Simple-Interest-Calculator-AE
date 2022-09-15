3403c176219db325993c0ac0e6bb14a95e088660
AndreaE86 <68388003+AndreaE86@users.noreply.github.com>
2022-09-15 11:57:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function compute() {
 	var year = new Date().getFullYear()+parseInt(years);
 
     	//displays message and complete output of calculation to user
-	document.getElementById("result").innerHTML= "\<br\>\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\> You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>"
+	document.getElementById("result").innerHTML= "\<br\>\<br\>If you deposit <mark>"+principal+"</mark>, <br\>at an interest rate of <mark>"+rate+"%</mark>.<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>"
 }
 	
 	
